509948bed952f34b9c67252aded30f61300f4c03
Aayush <just.another.3d.object@gmail.com>
2023-10-04 17:39:21 +0530

    Taking inputs from both the players

--- connectfour.py
+++ connectfour.py
@@ -4,7 +4,19 @@ import numpy as np
 def createBoard():
     return np.zeros((6,7))
 
-print(createBoard())
+board = createBoard()
+gameOver = False
+turn = 0
+
+while not gameOver:
+    # Ask for player 1 input
+    if turn == 0:
+        selection = int(input("Player 1 make your selection (0-6): "))
+
+    # Ask for player 2 input
+    else:
+        selection = int(input("Player 2 make your selection (0-6): "))
+
 
 
 
