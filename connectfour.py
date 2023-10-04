f69ae06951edccbd0fdf614e65f8039abf871b45
Aayush <just.another.3d.object@gmail.com>
2023-10-04 19:25:25 +0530

    Created game over logic

--- connectfour.py
+++ connectfour.py
@@ -1,4 +1,5 @@
 import numpy as np
+import itertools
 
 # Connect four board has dimentions 6*7
 ROW_COUNT = 6
@@ -11,10 +12,9 @@ board = createBoard()
 gameOver = False
 turn = 0
 
-
 # Checking if the top row of a selected column is empty or not
 def isValidLocation(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 # Getting the lowest empty slot of the selected column
 def getNextOpenRow(board, col):
@@ -26,8 +26,32 @@ def getNextOpenRow(board, col):
 def dropPiece(board, row, col, piece):
     board[row][col] = piece
 
+#  Flipping the board before printing it because numpy starts 0,0 index from top left instaed of bottom left
 def printBoard(board):
     print(np.flip(board, 0))
+
+# Checking if the game is over
+def gameOverCheck(board, piece):
+    
+    #  Checking horizontal win
+    for c, r in itertools.product(range(COLUMN_COUNT - 3), range(ROW_COUNT)):
+        if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+            return True
+
+    #  Checking vertical win
+    for c, r in itertools.product(range(COLUMN_COUNT), range(ROW_COUNT - 3)):
+        if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+            return True
+
+    #  Checking positive slop diagonal win
+    for c, r in itertools.product(range(COLUMN_COUNT - 3), range(ROW_COUNT - 3)):
+        if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+            return True
+        
+    #  Checking negative slop diagonal win
+    for c, r in itertools.product(range(COLUMN_COUNT - 3), range(3, ROW_COUNT)):
+        if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+            return True
 
 printBoard(board)
 #  Game loop
@@ -40,6 +64,10 @@ while not gameOver:
             row = getNextOpenRow(board, col)
             dropPiece(board, row, col, 1)
 
+            if gameOverCheck(board, 1):
+                print("Player 1 Wins!!")
+                gameOver = True
+
     # Ask for player 2 input
     else:
         col= int(input("Player 2 make your selection (0-6): "))
@@ -47,6 +75,10 @@ while not gameOver:
         if isValidLocation(board, col):
             row = getNextOpenRow(board, col)
             dropPiece(board, row, col, 2)
+
+            if gameOverCheck(board, 2):
+                print("Player 2 Wins!!")
+                gameOver = True
 
     printBoard(board)
 
