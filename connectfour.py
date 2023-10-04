8fd3fa7f9d80357c57db4abbb444d1ef99ca8647
Aayush <just.another.3d.object@gmail.com>
2023-10-04 19:39:50 +0530

    Implimenting graphics with pygame

--- connectfour.py
+++ connectfour.py
@@ -1,5 +1,7 @@
 import numpy as np
 import itertools
+import pygame
+import sys
 
 # Connect four board has dimentions 6*7
 ROW_COUNT = 6
@@ -53,38 +55,61 @@ def gameOverCheck(board, piece):
         if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
             return True
 
+def drawBoard(board):
+    pass
+
 printBoard(board)
+
+#  Initialising the game
+pygame.init()
+
+SQUARESIZE = 100
+
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT + 1) * SQUARESIZE
+
+size = (width, height)
+
+screen = pygame.display.set_mode(size)
+
 #  Game loop
 while not gameOver:
-    # Ask for player 1 input
-    if turn == 0:
-        col = int(input("Player 1 make your selection (0-6): "))
 
-        if isValidLocation(board, col):
-            row = getNextOpenRow(board, col)
-            dropPiece(board, row, col, 1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-            if gameOverCheck(board, 1):
-                print("Player 1 Wins!!")
-                gameOver = True
+        if event.type == pygame.MOUSEBUTTONDOWN:
 
-    # Ask for player 2 input
-    else:
-        col= int(input("Player 2 make your selection (0-6): "))
+            # Ask for player 1 input
+            # if turn == 0:
+            #     col = int(input("Player 1 make your selection (0-6): "))
 
-        if isValidLocation(board, col):
-            row = getNextOpenRow(board, col)
-            dropPiece(board, row, col, 2)
+            #     if isValidLocation(board, col):
+            #         row = getNextOpenRow(board, col)
+            #         dropPiece(board, row, col, 1)
 
-            if gameOverCheck(board, 2):
-                print("Player 2 Wins!!")
-                gameOver = True
+            #         if gameOverCheck(board, 1):
+            #             print("Player 1 Wins!!")
+            #             gameOver = True
 
-    printBoard(board)
+            # # Ask for player 2 input
+            # else:
+            #     col= int(input("Player 2 make your selection (0-6): "))
 
-    #  Switching turns between players
-    turn += 1
-    turn %= 2
+            #     if isValidLocation(board, col):
+            #         row = getNextOpenRow(board, col)
+            #         dropPiece(board, row, col, 2)
+
+            #         if gameOverCheck(board, 2):
+            #             print("Player 2 Wins!!")
+            #             gameOver = True
+
+            printBoard(board)
+
+            # #  Switching turns between players
+            # turn += 1
+            # turn %= 2
 
 
 
