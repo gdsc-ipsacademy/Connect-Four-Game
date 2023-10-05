a2a6ca1893871c2e0d0f4ec9729045f3c3ee91b1
Aayush <just.another.3d.object@gmail.com>
2023-10-06 00:02:06 +0530

    Fixed some bugs & increased preference for center

--- functions.py
+++ functions.py
@@ -68,63 +68,61 @@ def drawBoard(board):
 
     pygame.display.update()
 
-# Evaluating the score of connections 
+# Evaluating scores of connections
 def evaluateWindow(window, piece):
 
     score = 0
     opponentPiece = PLAYER_PIECE
     if piece == PLAYER_PIECE:
         opponentPiece = AI_PIECE
-
-    # If AI connects pieces
+    
     if window.count(piece) == 4:
         score += 100
-
     elif window.count(piece) == 3 and window.count(EMPTY) == 1:
         score += 50
-
     elif window.count(piece) == 2 and window.count(EMPTY) == 2:
         score += 10
 
-    # If human connects pieces
     if window.count(opponentPiece) == 3 and window.count(EMPTY) == 1:
-        score -= 90
-    
-    elif window.count(opponentPiece) == 3 and window.count(EMPTY) == 1:
-        score -= 45
-
+        score -= 75    
     elif window.count(opponentPiece) == 2 and window.count(EMPTY) == 2:
-        score -= 9
+        score -= 25
 
     return score
-    
-# Getting scores for connect 4s and connect 3s
+
+# Getting scores for connections
 def scorePosition(board, piece):
 
-    # Horizontal score
     score = 0
+
+    # Prefer center column for increased probablity of win
+    centerArray = [int(i) for i in list(board[ : , COLUMN_COUNT//2])]
+    centerCount = centerArray.count(piece)
+    score += centerCount*10
+
+    # Horizontal score
     for r in range(ROW_COUNT):
         rowArray = [int(i) for i in list(board[r , : ])]
         for c in range(COLUMN_COUNT - 3):
             window = rowArray[c : c + WINDOW_LENGTH]
-            score = evaluateWindow(window, piece)
+            score += evaluateWindow(window, piece)
 
     # Vertical score
     for c in range(COLUMN_COUNT):
         colArray = [int(i) for i in list(board[ : , c])]
         for r in range(ROW_COUNT):
             window = colArray[r : r + WINDOW_LENGTH]
-            score = evaluateWindow(window, piece)
+            score += evaluateWindow(window, piece)
 
     # Positive slope diagonal score
     for r, c in itertools.product(range(ROW_COUNT - 3), range(COLUMN_COUNT - 3)):
         window = [board[r + i][c + i] for i in range(WINDOW_LENGTH)]
-        score = evaluateWindow(window, piece)
+        score += evaluateWindow(window, piece)
 
     # Negative slope diagonal score
     for r, c in itertools.product(range(ROW_COUNT - 3), range(COLUMN_COUNT - 3)):
         window = [board[r + 3 - i][c + i] for i in range(WINDOW_LENGTH)]
-        score = evaluateWindow(window, piece)
+        score += evaluateWindow(window, piece)
 
     return score
 
